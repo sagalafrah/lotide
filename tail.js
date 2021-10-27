38d71d02e3953004e10159467b0f581b0226ee94
Sagal Afrah <afrahsagal@gmail.com>
2021-10-26 21:51:39 -0400

    implement tail

---
+++ tail.js
@@ -0,0 +1,7 @@
+const tail = function(array) {
+    return array.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
