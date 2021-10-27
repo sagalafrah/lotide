71a3ad1e9608bded7482fe55a76c56704905f7b1
Sagal Afrah <afrahsagal@gmail.com>
2021-10-27 15:34:17 -0400

    implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+    }
+  };
