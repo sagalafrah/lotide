48c64852594fe2c5e3575f7014edd507a6ec1bf2
Sagal Afrah <afrahsagal@gmail.com>
2021-10-28 22:16:44 -0400

    map testing code

---
+++ mapTest.js
@@ -0,0 +1,8 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const newLighthouses = lighthouses.map(function(element){
+    return element.length
+
+
+
+})
