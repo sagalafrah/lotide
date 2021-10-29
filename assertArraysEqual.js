34d3163ca0dff27b6951110a819f1bc02757739d
Sagal Afrah <afrahsagal@gmail.com>
2021-10-28 22:08:22 -0400

    fixed assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,14 @@
-const eqArrays = require("./eqArrays");
+function eqArrays(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
