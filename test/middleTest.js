9a347d086a382523d4a2460fddf4ec7817300821
Sagal Afrah <afrahsagal@gmail.com>
2021-11-02 00:05:05 -0400

    middle refactor

---
+++ test/middleTest.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [1, 2, 3] for [2]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+
+});
+
+
